5d9ccd7affca2aa96abe37c16bf78ee5a9aa4960
AChalumeau <achalumeau37000@gmail.com>
2022-10-21 14:10:28 +0200

    Update get_obsCoord

--- libstempo/libstempo.pyx
+++ libstempo/libstempo.pyx
@@ -1685,7 +1685,7 @@ cdef class tempopulsar:
         formResiduals(self.psr,self.npsr,1 if removemean else 0)
 
     def get_obsCoord(self):
-        return get_obsCoord(self.psr,self.npsr)
+        get_obsCoord(self.psr,self.npsr)
         
 
     # TO DO: proper dimensionfy as a table
